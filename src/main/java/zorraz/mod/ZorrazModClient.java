6ebc5ae94ba27c7d6e7892e3d96fbc091b32697a
Zortalo <rexzob35@gmail.com>
2025-04-10 17:29:04 -0700

    Add

---
+++ src/main/java/zorraz/mod/ZorrazModClient.java
@@ -0,0 +1,10 @@
+package zorraz.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ZorrazModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
